0889ecad38e333decfc66ae0d51e74ad7ea27570
Viral.pr <viral.p@devhostserver.com>
2023-02-01 18:57:13 +0530

    bug

--- src/Task-3/MainFile.js
+++ src/Task-3/MainFile.js
@@ -4,14 +4,14 @@ import './MainFile.css';
 function MainFile() {
 
     //using Usestate
-    const[newItem, setNewItem] = useState("");
-    const[items, setItems] = useState([]);
+    const [newItem, setNewItem] = useState("");
+    const [items, setItems] = useState([]);
 
 
     //Helper functions additem
     function addItem() {
 
-        if(!newItem){
+        if (!newItem) {
             alert("Please add an item then press Add");
             return;
         }
@@ -27,22 +27,23 @@ function MainFile() {
 
     function deleteItem(id) {
         const newArray = items.filter(item => item.id != id);
-        setItems(newArray);  
+        setItems(newArray);
     }
 
     return (
         <>
             {/* 1.Header */}
             <h1 className='container'>Todo List App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla nesciunt pariatur autem repudiandae aliquid, eius suscipit quam tempore eveniet officia?</p>
 
             {/* 2. Input elements button  */}
             <input type="text" placeholder='Add an item..' value={newItem} onChange={e => setNewItem(e.target.value)} />
-            <button onClick={() => addItem() }>Add</button>
+            <button onClick={() => addItem()}>Add</button>
 
             {/* 3.List of items (unordered list with list items) */}
             <ul>
                 {items.map(item => {
-                    return(
+                    return (
                         <li key={item.id}>{item.value} <button className='delete-button' onClick={() => deleteItem(item.id)}>❌</button></li>
                     )
                 })}
